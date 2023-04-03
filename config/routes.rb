00c55aab6a58eaeebfbb85e08e4686bc133a2b3c
maerzman <maerzingermanuel1310@gmail.com>
2023-04-03 15:58:02 +0200

    implementation of register and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root "main#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "registration", to: "registration#new"
+  post "registration", to: "registration#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 end
